a67377bedd6441bee9be6a88146a1fc6b7dd9db0
jeri <diltsjeri@gmail.com>
2021-01-29 14:50:23 -0500

    scaffolded tickets and messages

---
+++ TT_ticket_tracker/db/migrate/20210129194824_create_messages.rb
@@ -0,0 +1,13 @@
+class CreateMessages < ActiveRecord::Migration[6.1]
+  def change
+    create_table :messages do |t|
+      t.string :data
+      t.string :sd_id
+      t.integer :timestamp
+      t.string :clientID
+      t.references :tickets, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
