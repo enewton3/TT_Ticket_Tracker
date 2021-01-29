f23603b2853f823abb5911ab00224781380519a3
jeri <diltsjeri@gmail.com>
2021-01-29 15:24:43 -0500

    created seed file with 3 new users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Destroy! -----------------------
+
+User.destroy_all
+
+
+
+
+
+
+
+
+
+
+# Create Users! -----------------------
+
+@admin = User.create!(username: 'admin', email: 'admin@gmail.com', password_digest: 'password')
+
+user_list = [
+  {
+    username: "princess",
+    email: "princess@gmail.com",
+    password_digest: "password",
+  },
+  {
+    username: "guapo",
+    email: "guapo@gmail.com",
+    password_digest: "password",
+  }
+]
+
+User.create(user_list)
+
+## log
+puts "#{User.count} users created"
