12171356899e5f189483f604d6b7233e9a88a419
jeri <diltsjeri@gmail.com>
2021-01-29 15:33:44 -0500

    make user controller changes and tested with postman - everything is working :)

--- TT_ticket_tracker/app/controllers/users_controller.rb
+++ TT_ticket_tracker/app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
   # GET /users
   def index
     @users = User.all
-
     render json: @users
   end
 
@@ -16,7 +15,6 @@ class UsersController < ApplicationController
   # POST /users
   def create
     @user = User.new(user_params)
-
     if @user.save
       render json: @user, status: :created, location: @user
     else
