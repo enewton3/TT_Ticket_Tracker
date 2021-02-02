b902597b147d5964ee692dcc337f0b7898853bcd
jeri <diltsjeri@gmail.com>
2021-02-02 12:10:23 -0500

    got TicketDetail screen rendering - almost at full CRUD

--- TT_ticket_tracker/client/src/screens/TicketDetail/TicketDetail.jsx
+++ TT_ticket_tracker/client/src/screens/TicketDetail/TicketDetail.jsx
@@ -5,57 +5,59 @@ import { Button } from "@material-ui/core";
 import DeleteIcon from "@material-ui/icons/Delete";
 import "./TicketDetail.css";
 
-
-
 export default function TicketDetail(props) {
+  const [ticket, setTicket] = useState(null);
+  const [isLoaded, setLoaded] = useState(false);
+  const { id } = useParams();
+  const history = useHistory();
 
-    const [ticket, setTicket] = useState(null);
-    const { id } = useParams();
-    const history = useHistory();
-  
-    useEffect(() => {
-      const fetchTicket = async () => {
-        const tickett = await getTicket(id);
-        setTicket(ticket);
-      };
-      fetchTicket();
-    }, [id]);
-
-    const onTicketDelete = (e) => {
-        e.preventDefault();
-        deleteTicket(id).then(() => history.push("/tickets"));
+  useEffect(() => {
+    const fetchTicket = async () => {
+      const ticket = await getTicket(id);
+      console.log('here')
+      setTicket(ticket);
+      setLoaded(true);
     };
+    fetchTicket();
+  }, [id]);
 
-    return(
-        <div className="ticket-detail-div">
-            <div className="ticket-info">
-                <div className="ticket-id">{ticket.id}</div>
-                <div className="ticket-title">{ticket.title}</div>
-                <div className="ticket-description">{ticket.description}</div>
-                <div className="ticket-status">{ticket.status}</div>
-            </div>
+  if (!isLoaded) {
+    return <h1>Loading...</h1>;
+  }
+  const onTicketDelete = (e) => {
+    e.preventDefault();
+    deleteTicket(id).then(() => history.push("/tickets"));
+  };
 
-            {props.currentUser ? (
-            <div className="button-div">
-                <Button
-                    className="edit-button-detail"
-                    variant="contained"
-                    color="primary"
-                >
-                {/* <Link to={`/ticket/edit/${ticket.id}`}>Edit</Link> */}
-                <Link to={``}>Edit</Link>
-              </Button>
-              <Button
-                className="delete-button"
-                variant="contained"
-                onClick={onTicketDelete}
-                color="secondary"
-                startIcon={<DeleteIcon />}
-              >
-                Delete
-              </Button>
-            </div>
-        ): null }
+  return (
+    <div className="ticket-detail-div">
+      <div className="ticket-info">
+        <div className="ticket-id">{id}</div>
+        <div className="ticket-title">{ticket.title}</div>
+        <div className="ticket-description">{ticket.description}</div>
+        <div className="ticket-status">{ticket.status}</div>
+      </div>
+
+      {props.currentUser ? (
+        <div className="button-div">
+          <Button
+            className="edit-button-detail"
+            variant="contained"
+            color="primary"
+          >
+        <Link to={`/ticket/edit/${ticket.id}`}>Edit</Link>
+          </Button>
+          <Button
+            className="delete-button"
+            variant="contained"
+            onClick={onTicketDelete}
+            color="secondary"
+            startIcon={<DeleteIcon />}
+          >
+            Delete
+          </Button>
+        </div>
+      ) : null}
     </div>
-    )
+  );
 }
